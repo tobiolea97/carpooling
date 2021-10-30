8578b13ec62deb4ad5a4ca3c675dd860e33c194a
Tobias Olea <tolea@request.com.ar>
2021-10-30 19:07:56 -0300

    1) Filtrado de busqueda de viajes

--- app/src/main/java/utn/frgp/edu/ar/carpooling/Buscar.java
+++ app/src/main/java/utn/frgp/edu/ar/carpooling/Buscar.java
@@ -43,13 +43,14 @@ public class Buscar extends AppCompatActivity {
     String nombreUsuario, apellidoUsuario, emailUsuario, rolUsuario;
     GridView grillaViajes;
     Context context;
-    TextView filtroRecorrido, filtroRecorridoDestino, filtroFecha;
+    TextView filtroRecorrido, filtroRecorridoDestino, filtroFecha, filtroFechaQuery;
     Spinner spFiltroProvinciaOrigen, spFiltroCiudadesOrigen, spFiltroProvinciaDestino, spFiltroCiudadesDestino;
     ArrayList<String> listaCiudadesOrigen;
     List<Ciudad> itemsCiudadesOrigen;
     Provincia provOrigSelecc;
     List<Provincia> itemsProvincias;
     Chip chOrigen,chDestino, chFecha;
+    ArrayAdapter<String> adapterProvincias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +77,9 @@ public class Buscar extends AppCompatActivity {
         filtroRecorrido = (TextView) findViewById(R.id.textView19);
         filtroRecorridoDestino = (TextView) findViewById(R.id.textView20);
         filtroFecha = (TextView) findViewById(R.id.textView25);
+        filtroFechaQuery = (TextView) findViewById(R.id.textView22);
+        filtroFechaQuery.setText(" ");
+
 
         spFiltroProvinciaOrigen.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -197,6 +201,7 @@ public class Buscar extends AppCompatActivity {
                                 (month + 1 < 10 ? "0" + (month+1) : (month+1)) +"/" +
                                 year;
                 filtroFecha.setText("Despues del " + selectedDate);
+                filtroFechaQuery.setText(year + "-" + (month + 1) + "-" + day);
             }
         });
 
@@ -205,7 +210,43 @@ public class Buscar extends AppCompatActivity {
     }
 
     public void onClickBuscar(View view) {
-        
+
+        String filtro = "";
+        boolean flag = false;
+
+        if(!spFiltroProvinciaOrigen.getSelectedItem().equals(" ")) {
+            filtro += flag ? " AND " : " WHERE ";
+            filtro += " po.Nombre = '" + spFiltroProvinciaOrigen.getSelectedItem().toString() + "' ";
+            flag = true;
+        }
+
+        if(spFiltroCiudadesOrigen.getAdapter() != null && !spFiltroCiudadesOrigen.getSelectedItem().equals(" ")) {
+            filtro += flag ? " AND " : " WHERE ";
+            filtro += " co.Nombre = '" + spFiltroCiudadesOrigen.getSelectedItem().toString() + "' ";
+            flag = true;
+        }
+
+        if(spFiltroProvinciaDestino.getAdapter() != null && !spFiltroProvinciaDestino.getSelectedItem().equals(" ")) {
+            filtro += flag ? " AND " : " WHERE ";
+            filtro += " pd.Nombre = '" + spFiltroProvinciaDestino.getSelectedItem().toString() + "' ";
+            flag = true;
+        }
+
+        if(spFiltroCiudadesDestino.getAdapter() != null && !spFiltroCiudadesDestino.getSelectedItem().equals(" ")) {
+            filtro += flag ? " AND " : " WHERE ";
+            filtro += " cd.Nombre = '" + spFiltroCiudadesDestino.getSelectedItem().toString() + "' ";
+            flag = true;
+        }
+
+        if(!filtroFechaQuery.equals(" ")) {
+            filtro += flag ? " AND " : " WHERE ";
+            filtro += " vj.FechaHoraInicio > '" + filtroFechaQuery.getText() + "' ";
+        }
+
+        CargarViajesFiltrados task = new CargarViajesFiltrados(filtro);
+        task.execute();
+
+
     }
 
 
@@ -268,7 +309,7 @@ public class Buscar extends AppCompatActivity {
                     itemsProvincias.add(new Provincia(resultados.getInt("Id"),resultados.getString("Nombre"),resultados.getBoolean("EstadoRegistro")));
                 }
 
-                ArrayAdapter<String> adapterProvincias = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, provincias);
+                adapterProvincias = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, provincias);
                 adapterProvincias.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spFiltroProvinciaOrigen.setAdapter(adapterProvincias);
                 spFiltroProvinciaDestino.setAdapter(adapterProvincias);
@@ -400,5 +441,70 @@ public class Buscar extends AppCompatActivity {
         }
     }
 
+    private class CargarViajesFiltrados extends AsyncTask<String,Integer, ResultSet> {
+
+        String filtro;
+
+        public CargarViajesFiltrados(String _filtro) {
+            this.filtro = _filtro;
+        }
+
+        @Override
+        protected ResultSet doInBackground(String... queries) {
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(DataDB.urlMySQL, DataDB.user, DataDB.pass);
+                Statement st = con.createStatement();
+
+                String query = "";
+                query += " SELECT vj.Id, po.Nombre ProvinciaOrigen, co.Nombre CiudadOrigen, pd.Nombre ProvinciaDestino, cd.Nombre CiudadDestino, vj.FechaHoraInicio, vj.EstadoViaje ";
+                query += " FROM Viajes vj ";
+                query += " INNER JOIN Provincias po ";
+                query += " 	ON vj.ProvinciaOrigenId = po.Id ";
+                query += " INNER JOIN Ciudades co ";
+                query += " 	ON vj.CiudadOrigenId = co.Id ";
+                query += " INNER JOIN Provincias pd ";
+                query += " 	ON vj.ProvinciaDestinoId = pd.Id ";
+                query += " INNER JOIN Ciudades cd ";
+                query += " 	ON vj.CiudadDestinoId = cd.Id ";
+                query += filtro;
+
+                return st.executeQuery(query);
+
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(ResultSet resultados) {
+            super.onPostExecute(resultados);
+            try {
+                List<Map<String, String>> itemsGrilla = new ArrayList<Map<String, String>>();
+
+                while (resultados.next()) {
+                    Map<String, String> item = new HashMap<String, String>();
+                    item.put("NroViaje", resultados.getString("Id"));
+                    item.put("origen", resultados.getString("CiudadOrigen") + ", " + resultados.getString("ProvinciaOrigen"));
+                    item.put("destino", resultados.getString("CiudadDestino") + ", " + resultados.getString("ProvinciaDestino"));
+                    item.put("fecha", resultados.getString("FechaHoraInicio").substring(8,10) + "/" + resultados.getString("FechaHoraInicio").substring(5,7) + "/" + resultados.getString("FechaHoraInicio").substring(2,4));
+                    item.put("hora", resultados.getString("FechaHoraInicio").substring(11,13) + ":" + resultados.getString("FechaHoraInicio").substring(14,16));
+                    item.put("estado",resultados.getString("EstadoViaje"));
+                    itemsGrilla.add(item);
+                }
+
+                String[] from = {"NroViaje","origen", "destino", "fecha", "hora","estado"};
+                int[] to = {R.id.tvGridItemViajeNroViaje,R.id.tvGridItemViajeOrigen, R.id.tvGridItemViajeDestino, R.id.tvGridItemViajeOrigenFecha, R.id.tvGridItemViajeOrigenHora,R.id.tvGridItemEstadoViaje};
+                SimpleAdapter simpleAdapter = new SimpleAdapter(context, itemsGrilla, R.layout.grid_item_viaje, from, to);
+                grillaViajes.setAdapter(simpleAdapter);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
