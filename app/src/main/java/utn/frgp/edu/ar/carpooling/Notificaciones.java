2679de97205eccffd5cd44315152c547626d2299
Franco009 <57353300+Franco009@users.noreply.github.com>
2021-11-08 02:11:25 -0300

    Notificaciones, Implementando mensajes leidos y no leidos

--- app/src/main/java/utn/frgp/edu/ar/carpooling/Notificaciones.java
+++ app/src/main/java/utn/frgp/edu/ar/carpooling/Notificaciones.java
@@ -35,7 +35,8 @@ import utn.frgp.edu.ar.carpooling.conexion.DataDB;
 import utn.frgp.edu.ar.carpooling.negocioImpl.NotificacionesNegImpl;
 
 public class Notificaciones extends AppCompatActivity {
-    ListView LvNotificacion;
+    ListView LvNotificacionLeidos;
+    ListView LvNotificacionNoLeidos;
     Context contexto;
     String nombreUsuario, apellidoUsuario, emailUsuario, rolUsuario, idUsuario;
     @Override
@@ -43,7 +44,8 @@ public class Notificaciones extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificaciones);
         contexto = this;
-        LvNotificacion=findViewById(R.id.LvNotificaciones);
+        LvNotificacionLeidos=findViewById(R.id.LvNotificacionesLeidos);
+        LvNotificacionNoLeidos=findViewById(R.id.LvNotificaciones);
         SharedPreferences spSesion = getSharedPreferences("Sesion", Context.MODE_PRIVATE);
         nombreUsuario = spSesion.getString("Nombre","No hay datos");
         apellidoUsuario = spSesion.getString("Apellido","No hay datos");
@@ -62,6 +64,9 @@ public class Notificaciones extends AppCompatActivity {
 
         NotificationManagerCompat notificationManagerCompat= NotificationManagerCompat.from(getApplicationContext());
         notificationManagerCompat.cancel(NOTIFICACION_ID);
+
+
+        /*
   ArrayList<String> itemListt= new ArrayList<String>();
         itemListt.add("Item1"+"-P");
         itemListt.add("Item2"+"-L");
@@ -72,13 +77,13 @@ public class Notificaciones extends AppCompatActivity {
 "Item3",
 
 };*/
-       
 
-ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(contexto,R.layout.list_item_viajes,itemListt){
 
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(contexto,R.layout.list_item_viajes,itemListt){
+
+  //  @NonNull
+    //@Override
+   /* public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
         View view=super.getView(position, convertView, parent);
         for (String o : itemListt){
@@ -95,7 +100,7 @@ ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(contexto,R.layout.li
             }
             System.out.println("2 pasa una vez");
         }
-     //  if(/*position%2==1*/itemListt.toString().equals("Item1")){
+     //  if(/*position%2==1*///itemListt.toString().equals("Item1")){
 
 
        // }else{
@@ -107,14 +112,15 @@ ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(contexto,R.layout.li
 
 
 
-return view;
-    }
-};
+//return view;
+  //  }
+//};
 
 
-        LvNotificacion.setAdapter(arrayAdapter);
+//        LvNotificacion.setAdapter(arrayAdapter);
 
-      //  new CargarNotificaciones().execute();
+        new CargarNotificaciones().execute();
+        new CargarNotificacionesnoLeidos().execute();
     }
 
     @Override
@@ -212,7 +218,7 @@ return view;
                 String query = "";
                 query += " SELECT 	noti.Mensaje";
                 query += " FROM Notificaciones noti";
-                query += " 	Where	noti.UsuarioId=" + idUsuario;
+                query += " 	Where	noti.EstadoNotificacion='L' and noti.UsuarioId=" + idUsuario;
 
                 return st.executeQuery(query);
 
@@ -238,7 +244,55 @@ return view;
 
 
                 ArrayAdapter<String> adapter= new ArrayAdapter<>(contexto,R.layout.list_item_viajes,Mensajes);
-                LvNotificacion.setAdapter(adapter);
+                LvNotificacionLeidos.setAdapter(adapter);
+
+
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private class CargarNotificacionesnoLeidos extends AsyncTask<Void,Integer, ResultSet> {
+        @Override
+        protected ResultSet doInBackground(Void... voids) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(DataDB.urlMySQL, DataDB.user, DataDB.pass);
+                Statement st = con.createStatement();
+                String query = "";
+                query += " SELECT 	noti.Mensaje";
+                query += " FROM Notificaciones noti";
+                query += " 	Where	noti.EstadoNotificacion='P' and noti.UsuarioId=" + idUsuario;
+
+                return st.executeQuery(query);
+
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ResultSet resultados) {
+            super.onPostExecute(resultados);
+            try {
+                ArrayList<String> Mensajes= new ArrayList<String>();
+
+
+                while (resultados.next()) {
+
+                    Mensajes.add(resultados.getString("Mensaje"));
+
+                }
+
+
+
+                ArrayAdapter<String> adapter= new ArrayAdapter<>(contexto,R.layout.list_item_viajes,Mensajes);
+                LvNotificacionNoLeidos.setAdapter(adapter);
 
                 new MensajeLeido().execute();
 
