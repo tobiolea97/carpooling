ff126ef7c5f7b7c4bed5f805460d928b372e1066
Tobias Olea <tolea@request.com.ar>
2021-11-10 00:38:16 -0300

    Ajustes varios

--- app/src/main/java/utn/frgp/edu/ar/carpooling/VerSolicitud.java
+++ app/src/main/java/utn/frgp/edu/ar/carpooling/VerSolicitud.java
@@ -2,13 +2,116 @@ package utn.frgp.edu.ar.carpooling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import utn.frgp.edu.ar.carpooling.conexion.DataDB;
 
 public class VerSolicitud extends AppCompatActivity {
+
+    Context contexto;
+    GridView grillaverViaje;
+    String nroSolicitud;
+    String nombreUsuario, apellidoUsuario, emailUsuario, rolUsuario, idUsuario, estadoViaje, localDateviaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_solicitud);
+        contexto = this;
+
+        SharedPreferences spSesion = getSharedPreferences("Sesion", Context.MODE_PRIVATE);
+        nombreUsuario = spSesion.getString("Nombre","No hay datos");
+        apellidoUsuario = spSesion.getString("Apellido","No hay datos");
+        emailUsuario = spSesion.getString("Email","No hay datos");
+        rolUsuario = spSesion.getString("Rol","No hay datos");
+        idUsuario = spSesion.getString("Id","No hay datos");
+
+        nroSolicitud = getIntent().getStringExtra("NroSolicitud");
+
+        grillaverViaje = findViewById(R.id.GrVerSolicitud);
+
+        new CargarSolicitudSeleccionada().execute();
+
     }
+
+    private class CargarSolicitudSeleccionada extends AsyncTask<Void,Integer, ResultSet> {
+
+        @Override
+        protected ResultSet doInBackground(Void... voids) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager.getConnection(DataDB.urlMySQL, DataDB.user, DataDB.pass);
+                Statement st = con.createStatement();
+                String query = "";
+                query += " SELECT 	vj.FechaHoraInicio,";
+                query += "  	    vj.Id,";
+                query += " 		    pr1.Nombre ProvinciaOrigen,";
+                query += "          ci1.Nombre CiudadOrigen,";
+                query += "          pr2.Nombre ProvinciaDestino,";
+                query += "          ci2.Nombre CiudadDestino,";
+                query += "          vj.EstadoSolicitud";
+                query += " FROM Solicitudes vj";
+                query += " LEFT JOIN Provincias pr1";
+                query += " 	ON pr1.Id = vj.ProvinciaOrigenId";
+                query += " LEFT JOIN Provincias pr2";
+                query += " 	ON pr2.Id = vj.ProvinciaDestinoId";
+                query += " LEFT JOIN Ciudades ci1";
+                query += " 	ON ci1.Id = vj.CiudadOrigenId";
+                query += " LEFT JOIN Ciudades ci2";
+                query += " 	ON ci2.Id = vj.CiudadDestinoId";
+                query += " 	Where	vj.Id='" + nroSolicitud + "'";
+
+                return st.executeQuery(query);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ResultSet resultados) {
+            super.onPostExecute(resultados);
+            try {
+                List<Map<String, String>> itemsGrilla = new ArrayList<Map<String, String>>();
+
+                while (resultados.next()) {
+
+                    Map<String, String> item = new HashMap<String, String>();
+                    item.put("NroSolicitud", resultados.getString("Id"));
+                    item.put("origen", resultados.getString("CiudadOrigen") + ", " + resultados.getString("ProvinciaOrigen"));
+                    item.put("destino", resultados.getString("CiudadDestino") + ", " + resultados.getString("ProvinciaDestino"));
+                    item.put("fecha", resultados.getString("FechaHoraInicio").substring(8,10) + "/" + resultados.getString("FechaHoraInicio").substring(5,7) + "/" + resultados.getString("FechaHoraInicio").substring(2,4));
+                    item.put("hora", resultados.getString("FechaHoraInicio").substring(11,13) + ":" + resultados.getString("FechaHoraInicio").substring(14,16));
+                    item.put("estado", resultados.getString("EstadoSolicitud"));
+
+                    itemsGrilla.add(item);
+                }
+
+                String[] from = {"NroSolicitud","origen", "destino", "fecha", "hora","estado"};
+                int[] to = {R.id.tvGridItemViajeNroViaje,R.id.tvGridItemViajeOrigen, R.id.tvGridItemViajeDestino, R.id.tvGridItemViajeOrigenFecha, R.id.tvGridItemViajeOrigenHora, R.id.tvGridItemEstadoViaje};
+                SimpleAdapter simpleAdapter = new SimpleAdapter(contexto, itemsGrilla, R.layout.grid_item_viaje, from, to);
+                grillaverViaje.setAdapter(simpleAdapter);
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
